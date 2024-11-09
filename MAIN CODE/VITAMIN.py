9567c8a1aa86e555f9a6032a8096d29368406a23
Pythonforbasics <114282587+Themaster1112@users.noreply.github.com>
2024-11-09 10:55:44 +0000

    first commit

--- MAIN CODE/VITAMIN.py
+++ MAIN CODE/VITAMIN.py
@@ -1,5 +1,5 @@
-input("Have you taken your vitamins? Yes or no?")
-if input == "Yes":
+VITAMINS=input("Have you taken your vitamins? Yes or no?")
+if VITAMINS == "Yes":
     print("You have taken your vitamins")
 else:
     print("Take your vitamins")
